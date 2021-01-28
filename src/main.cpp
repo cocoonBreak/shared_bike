ae2fa814d1c1dec1282d1dbc322dceaa607eaf51
cocoonBreak <1451624330@qq.com>
2021-01-28 16:32:59 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,28 @@
 #include <stdio.h>
 #include "common/iniconfig.h"
+#include "common/logger.h"
 
 int main(int argc, char **argv)
 {
-	if(argc != 2){
-		printf("Please input shbk <config file path>!\n");
+	if(argc != 3){
+		printf("Please input shbk <config file path> <log file config>!\n");
 		return -1;
 	}
 	
-	Iniconfig iniconf;
-	if(iniconf.loadfile(argv[1])){
-		const st_env_config &st_conf = iniconf.getconfig();
-		printf("db_ip: %s\n", st_conf.db_ip.c_str());
-		printf("db_port: %d\n", st_conf.db_port);
-		printf("db_user: %s\n", st_conf.db_user.c_str());
-		printf("db_pwd: %s\n", st_conf.db_pwd.c_str());
-		printf("db_name: %s\n", st_conf.db_name.c_str());
-		printf("svr_port: %d\n", st_conf.svr_port);
+	if(!logger::get_instance().init(std::string(argv[2]))){
+		fprintf(stderr, "init log module failed.\n");
+		return -2;
 	}
 	
+	Iniconfig iniconf;
+	if(!iniconf.loadfile(argv[1])){
+		LOG_ERROR("log %s failed.", argv[1]);
+		return -3;
+	}
+	
+	const st_env_config &st_conf = iniconf.getconfig();
+	LOG_INFO("[datebase] ip: %s port: %d user: %s pwd: %s name: %s  [server] port:%d\n", st_conf.db_ip.c_str(), st_conf.db_port, \
+			st_conf.db_user.c_str(), st_conf.db_pwd.c_str(), st_conf.db_name.c_str(), st_conf.svr_port);
+
 	return 0;
 }
